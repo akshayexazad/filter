9ee0112ab8ab9f044e2a1cdc2e5b50c958d25d20
akshay <akshayexazad@gmail.com>
2022-09-22 16:50:10 +0530

    bookapointment

--- filter.js
+++ filter.js
@@ -1,3 +1,34 @@
+// delete element .............
+let del=document.getElementById('btn');
+del.addEventListener('click', deleteone)
+
+function  deleteone(){
+let ulc=document.getElementById('ulc')
+let ele=  ulc.lastElementChild;
+ulc.removeChild(ele);
+}
+
+// addelement.............
+let add =document.getElementById('btn-add');
+add.addEventListener('click' , addone)
+
+function addone(){
+
+let iptxt=document.getElementById('textip').value;
+let addid=document.getElementById('ulc');
+let  elmcreate=document.createElement('li');
+let addtext=document.createTextNode(iptxt)
+elmcreate.appendChild(addtext);
+addid.appendChild(elmcreate)
+
+let iptxt2=document.getElementById('textip2').value;
+let addid2=document.getElementById('ulc');
+let addtext2=document.createTextNode(iptxt2);
+elmcreate.appendChild(addtext2);
+addid.appendChild(elmcreate);
+
+}
+//filter.............
 let filter= document.getElementById('filter');
   //filter event
   filter.addEventListener('keyup',filteritem);
@@ -14,7 +45,5 @@ let filter= document.getElementById('filter');
         }else{
           liselect[i].style.display='none';
         }
-
-   }
-
-  }
+     }
+ };
